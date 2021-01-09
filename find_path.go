4cac1205c825f037ff4a27d5f163b61aed5992e4
Janos Seteny <jseteny@gmail.com>
2021-01-09 21:41:51 +0100

    makeCalcList() which implements the Partial Application functional programming concept

--- find_path.go
+++ find_path.go
@@ -15,10 +15,11 @@ func main() {
 
 // FindPath find files and directories matching the given pattern
 func FindPath(pattern string) {
+	calcList := makeCalcList(pattern)
 	dirs := make([]string, 0)
 	for dir := range collectDirs(".") {
 		dirs = append(dirs, dir)
-		list := calcList(pattern, dirs)
+		list := calcList(dirs)
 		fmt.Println()
 		fmt.Printf("%q\n", list)
 	}
@@ -39,17 +40,23 @@ func collectDirs(root string) <-chan string {
 	return dirs
 }
 
-func calcList(path string, dirs []string) []string {
+// makeCalcList implements the Partial Application functional programming
+// concept. In this case it makes it possible to optimize out the
+// unnecessary strings.ReplaceAll() calls.
+func makeCalcList(path string) func(dirs []string) []string {
 	exp := strings.ReplaceAll(path, "/", ".*/.*")
-	result := make([]string, 0)
-	for _, dir := range dirs {
-		ok, err := regexp.MatchString(exp, dir)
-		if err != nil {
-			panic(err)
+
+	return func(dirs []string) []string {
+		result := make([]string, 0)
+		for _, dir := range dirs {
+			ok, err := regexp.MatchString(exp, dir)
+			if err != nil {
+				panic(err)
+			}
+			if ok {
+				result = append(result, dir)
+			}
 		}
-		if ok {
-			result = append(result, dir)
-		}
+		return result
 	}
-	return result
 }
